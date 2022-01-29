446ac37053ccbcc0d68da3b3232a4234e1e9be2e
nimrod vain <togithub1@gmail.com>
2022-01-29 19:49:29 +0200

    create nav bar logo

--- features/logo/logo.tsx
+++ features/logo/logo.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import texts from './logo.texts.json';
 import styles from './logo.module.scss';
 
 export const Logo = () => {
-  return <div className={styles.Logo}>Logo</div>;
+  return (
+    <div className={styles.Logo}>
+      <span className={styles.text}>{texts.text}</span>
+    </div>
+  );
 };
