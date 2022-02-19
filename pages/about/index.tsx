fb12318ed706e001921f2501150ca43499d7b4ff
nimrod vain <togithub1@gmail.com>
2022-02-19 17:39:10 +0200

    make about page to be the main page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,9 @@
-import { About } from '../features/about';
-import { ProfessionalSkillsSection } from '../features/professional-skills/professional-skills-section';
-import { Portfolio } from '../features/portfolio';
-import { Contact } from '../features/contact';
-import styles from '../styles/Home.module.scss';
-import { Footer } from '../components/footer';
+import { About } from '../../features/about';
+import { ProfessionalSkillsSection } from '../../features/professional-skills/professional-skills-section';
+import { Portfolio } from '../../features/portfolio';
+import { Contact } from '../../features/contact';
+import styles from '../../styles/Home.module.scss';
+import { Footer } from '../../components/footer';
 
 const Index = () => {
   return (
